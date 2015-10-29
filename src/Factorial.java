64e7747ef855d34b49810ff3969c4abb58d44541
dupanov <vadim.dupanov@gmail.com>
2015-10-29 22:37:13 +0200

    Initial commit

---
+++ src/Factorial.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Вадик on 29.10.2015.
+ */
+public class Factorial {
+    public static int factorial (int n){
+        if (n==1){
+            return 1;
+        } else {
+            return n*factorial(n-1);
+        }
+    }
+}
